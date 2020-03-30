690ead3ca23c8e1c47393a6327680e6fbbb4b8d2
Oleh Karnaukhov <oleh.karnaukhov@playtika.com>
2020-03-30 09:06:12 +0300

    UTT and 10 Commandments

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,6 +1,14 @@
 <template>
   <v-container>
-    <p>{{ this.content ? this.content.name : "" }}</p>
+    <v-carousel show-arrows :continuous="false" delimiter-icon="mdi-circle-slice-8">
+      <v-carousel-item v-for="verse in this.content.verses" :key="verse.id">
+      <v-sheet color="green" height="100%" >
+        <v-row class="fill-height" align="center" justify="center">
+          <div>{{ verse.text[0] }}</div>
+        </v-row>
+      </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </v-container>
 </template>
 
